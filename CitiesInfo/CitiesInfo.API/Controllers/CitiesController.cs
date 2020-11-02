f86bf65239a63592499033a0ecf08e3ec9b3edf3
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-02 18:51:28 +0500

    applied IActionResult with Status Code in place of JSONResult

--- CitiesInfo/CitiesInfo.API/Controllers/CitiesController.cs
+++ CitiesInfo/CitiesInfo.API/Controllers/CitiesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace CitiesInfo.API.Controllers
 {
@@ -11,16 +13,32 @@ namespace CitiesInfo.API.Controllers
     {
         //[HttpGet("api/cities")] // Indivisual Route, has to be specified for every single request
         [HttpGet]
-        public JsonResult GetCities()
+        //public JsonResult GetCities()
+        //{
+        //    return new JsonResult(CitiesDataStore.StaticDataStoreObj.Cities);
+        //}
+        public IActionResult GetCities()
         {
-            return new JsonResult(CitiesDataStore.StaticDataStoreObj.Cities);
+            return Ok(CitiesDataStore.StaticDataStoreObj.Cities);
         }
 
         [HttpGet("{id}")]
-        public JsonResult GetCity(int id)
+
+        //public JsonResult GetCity(int id)
+        //{
+        //    return new JsonResult(CitiesDataStore.StaticDataStoreObj.Cities.FirstOrDefault(city => city.Id == id));
+        //    //FirstOrDefault is a LINQ Function that returns the first element that satisfies the condition or default.
+        //}
+
+        public IActionResult GetCity(int id)
         {
-            return new JsonResult(CitiesDataStore.StaticDataStoreObj.Cities.FirstOrDefault(city => city.Id == id));
-            //FirstOrDefault is a LINQ Function that returns the first element that satisfies the condition or default.
+            var CityToReturn = CitiesDataStore.StaticDataStoreObj.Cities.FirstOrDefault(city => city.Id == id);
+
+            if(CityToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(CityToReturn);
         }
 
         //public string Message()
