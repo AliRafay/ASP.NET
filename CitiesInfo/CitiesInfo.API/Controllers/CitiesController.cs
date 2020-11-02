20a024ed56f313582746fd55c96b348e85b000bd
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-02 15:00:08 +0500

    Created GetCities() httpGet request

---
+++ CitiesInfo/CitiesInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CitiesInfo.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpPost("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>(){
+                new {id=1 , Name="Karachi"},
+                new {id=2 , Name="Lahore"},
+                new {id=3 , Name="Islamabad"},
+            });
+        }
+        //public string Message()
+        //{
+        //    return "hello";
+        //}
+    }
+}
