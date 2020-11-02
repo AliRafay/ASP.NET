0e01d64540555ed642d3364aebbdce86dcfb46ce
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-02 16:08:17 +0500

    Created CityDto class in Models

--- CitiesInfo/CitiesInfo.API/Controllers/CitiesController.cs
+++ CitiesInfo/CitiesInfo.API/Controllers/CitiesController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace CitiesInfo.API.Controllers
 {
-    [Route("api/[Controller]")] //Controller = Cities
+    //[Route("api/[Controller]")] //Controller = Cities
     // OR //
-    //[Route("api/cities")] //do this for all Requests or do Indivisually 
+    [Route("api/cities")] //do this for all Requests or do Indivisually 
     [ApiController]
     public class CitiesController : ControllerBase
     {
@@ -14,11 +14,13 @@ namespace CitiesInfo.API.Controllers
         public JsonResult GetCities()
         {
             return new JsonResult(
-                new List<object>(){
+                new List<object>()
+                {
                 new {id=1 , Name="Karachi"},
                 new {id=2 , Name="Lahore"},
                 new {id=3 , Name="Islamabad"},
-            });
+                }
+                );
         }
         //public string Message()
         //{
