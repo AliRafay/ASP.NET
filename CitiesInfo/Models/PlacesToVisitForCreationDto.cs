1a0fe43085d0712cdae63089463700515bd4e1d8
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-03 15:30:37 +0500

    Created PlacesToVisitForCreationDto class in models

---
+++ CitiesInfo/Models/PlacesToVisitForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class PlacesToVisitForCreationDto
+    {
+        //without id because we dont want the consumer to provide id
+        public string Name { get; set; }
+    }
+}
