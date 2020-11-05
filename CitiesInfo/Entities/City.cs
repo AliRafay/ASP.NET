d2ea588f529cc65bc2e28faffa917dc5aa912463
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-05 17:48:41 +0500

    Created Entities Class Libraries with 2 entities
    PlacesToVisit and City entities created

---
+++ CitiesInfo/Entities/City.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //generates automatic id in accordance with SQLServer
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public List<PlacesToVisit> PlacesToVisit { get; set; } = new List<PlacesToVisit>();
+    }
+}
---
+++ CitiesInfo/Entities/PlacesToVisit.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class PlacesToVisit
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //generates automatic id in accordance with SQLServer
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        //[MaxLength(200)]
+        //public string Description { get; set; } //use later by using migrations
+
+        [ForeignKey("CityId")]
+        public City City { get; set; } //navigation property; this will connect placesToVisit to City, All others are column names
+        public int CityId { get; set; }
+    }
+}
