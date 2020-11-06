86d6e7a98b7720eed4d2718c05bf7568127e777f
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-06 17:27:17 +0500

    commented dummydata after database update with sampleData migration

--- CitiesInfo/Contexts/CityInfoContext.cs
+++ CitiesInfo/Contexts/CityInfoContext.cs
@@ -19,83 +19,84 @@ namespace Contexts
             //We can also create database by Update-Database command in package manager console
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            City[] Cities =
-            {
-                new City()
-                {
-                    Id=1,
-                    Name="Karachi",
-                    Description="City of Lights"
-                },
-                new City()
-                {
-                    Id=2,
-                    Name="Lahore",
-                    Description="Orange train wali city"
-                },
-                new City()
-                {
-                    Id=3,
-                    Name="Jamshoro",
-                    Description="City of Text Books"
-                }
-            };
-            PlaceToVisit[] PlacesToVisit = {
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    City[] Cities =
+        //    {
+        //        new City()
+        //        {
+        //            Id=1,
+        //            Name="Karachi",
+        //            Description="City of Lights"
+        //        },
+        //        new City()
+        //        {
+        //            Id=2,
+        //            Name="Lahore",
+        //            Description="Orange train wali city"
+        //        },
+        //        new City()
+        //        {
+        //            Id=3,
+        //            Name="Jamshoro",
+        //            Description="City of Text Books"
+        //        }
+        //    };
+        //    PlaceToVisit[] PlacesToVisit = {
 
-                new PlaceToVisit()
-                {
-                    CityId = 1,
-                    Id = 1,
-                    Name = "Chunky Monkey",
-                    Description = "Amusement Park"
-                },
-                new PlaceToVisit()
-                {
-                    CityId = 1,
-                    Id = 2,
-                    Name = "Mazar e Quaid",
-                    Description = "Mohammad Ali Jinnah Grave"
-                },
-                new PlaceToVisit()
-                {
-                    CityId = 2,
-                    Id = 3,
-                    Name = "Orange Train",
-                    Description = "PMLN's train plan"
-                },
-                new PlaceToVisit()
-                {
-                    CityId = 2,
-                    Id = 4,
-                    Name = "Badshahi Mosque",
-                    Description = "red mosque"
-                },
-                new PlaceToVisit()
-                {
-                    CityId = 2,
-                    Id = 5,
-                    Name = "Lahore Fort",
-                    Description = "An old fort of Lahore"
-                },
-                new PlaceToVisit()
-                {
-                    CityId = 3,
-                    Id = 6,
-                    Name = "Rani Kot Fort",
-                    Description = "Fort of Rani Kot"
+        //        new PlaceToVisit()
+        //        {
+        //            CityId = 1,
+        //            Id = 1,
+        //            Name = "Chunky Monkey",
+        //            Description = "Amusement Park"
+        //        },
+        //        new PlaceToVisit()
+        //        {
+        //            CityId = 1,
+        //            Id = 2,
+        //            Name = "Mazar e Quaid",
+        //            Description = "Mohammad Ali Jinnah Grave"
+        //        },
+        //        new PlaceToVisit()
+        //        {
+        //            CityId = 2,
+        //            Id = 3,
+        //            Name = "Orange Train",
+        //            Description = "PMLN's train plan"
+        //        },
+        //        new PlaceToVisit()
+        //        {
+        //            CityId = 2,
+        //            Id = 4,
+        //            Name = "Badshahi Mosque",
+        //            Description = "red mosque"
+        //        },
+        //        new PlaceToVisit()
+        //        {
+        //            CityId = 2,
+        //            Id = 5,
+        //            Name = "Lahore Fort",
+        //            Description = "An old fort of Lahore"
+        //        },
+        //        new PlaceToVisit()
+        //        {
+        //            CityId = 3,
+        //            Id = 6,
+        //            Name = "Rani Kot Fort",
+        //            Description = "Fort of Rani Kot"
 
-                }
-            };
-            modelBuilder.Entity<City>()     //For Manual Addition in database
-                    .HasData(Cities);
+        //        }
+        //    };
+        //    modelBuilder.Entity<City>()     //For Manual Addition in database
+        //            .HasData(Cities);
 
-            modelBuilder.Entity<PlaceToVisit>()     //For Manual Addition in database
-                    .HasData(PlacesToVisit);
+        //    modelBuilder.Entity<PlaceToVisit>()     //For Manual Addition in database
+        //            .HasData(PlacesToVisit);
 
-            base.OnModelCreating(modelBuilder);
-        }
+        //    base.OnModelCreating(modelBuilder);
+
+        //}  //code for addition of dummydata manually
 
         DbSet<City> Cities { get; set; }
         DbSet<PlaceToVisit> PlacesToVisit { get; set; }
