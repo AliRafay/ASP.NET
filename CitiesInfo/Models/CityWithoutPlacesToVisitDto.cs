5c622646aaa61d40f4d9b2e5b3dee708753cd92b
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-09 15:31:34 +0500

    Create CityWithoutPlacesToVisitDto.cs

---
+++ CitiesInfo/Models/CityWithoutPlacesToVisitDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    //we put a choice for placesToVisit , if we use cityDto then there will be a empty placesToVisit and number of placesToVisit
+    public class CityWithoutPlacesToVisitDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
