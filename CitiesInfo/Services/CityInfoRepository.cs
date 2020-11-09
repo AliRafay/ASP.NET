27c8f3283071ca49777a76ce0c43104db62b11d4
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-09 15:17:04 +0500

    created CityInfoRepository

---
+++ CitiesInfo/Services/CityInfoRepository.cs
@@ -0,0 +1,50 @@
+﻿using Contexts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext c)
+        {
+            _context = c ??
+                throw new ArgumentNullException(nameof(c));
+        }
+        public City GetCity(int cityId, bool includePlacesToVisit)
+        {
+            if (includePlacesToVisit)
+            {
+                return _context.Cities
+                    .Include(c => c.PlacesToVisit)
+                    .Where(c => c.Id == cityId)
+                    .FirstOrDefault();
+            }
+            return _context.Cities
+                .Where(c => c.Id == cityId)
+                .FirstOrDefault();
+        }
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities.ToList();
+        }
+
+        public IEnumerable<PlaceToVisit> GetPlacesToVisitForCity(int cityId)
+        {
+            return _context.PlacesToVisit
+                 .Where(p => p.CityId == cityId).ToList();
+        }
+
+        public PlaceToVisit GetPlaceToVisitForCity(int id, int cityId)
+        {
+            return _context.PlacesToVisit
+                .Where(p=>p.Id == id && p.CityId == cityId).FirstOrDefault();
+        }
+
+    }
+}
---
+++ CitiesInfo/Services/ICityInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePlacesToVisit);
+        IEnumerable<PlaceToVisit> GetPlacesToVisitForCity(int cityId);
+        PlaceToVisit GetPlaceToVisitForCity(int id, int cityId);
+    }
+}
