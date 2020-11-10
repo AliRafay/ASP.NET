047cf3f08c9ad77d9f24111f513b198226501be7
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-10 14:49:47 +0500

    created Map in PlaceToVisitProfile

---
+++ CitiesInfo/Profiles/PlaceToVisitProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Entities;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Profiles
+{
+    public class PlaceToVisitProfile : Profile
+    {
+        public PlaceToVisitProfile()
+        {
+            CreateMap<PlaceToVisit, PlacesToVisitDto>();
+        }
+    }
+}
