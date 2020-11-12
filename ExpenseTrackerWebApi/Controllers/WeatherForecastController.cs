ec2bcebde6eea1474b9a77862a28e0005485568e
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-11-12 16:25:41 +0500

    Revert "deleted ExpenseTrackerAPi"
    This reverts commit 1fd295b12d4dc1eeb893d247b06d44e9983c6b38.

---
+++ ExpenseTrackerWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ExpenseTrackerWebApi.Controllers
+{
+    [ApiController]
+    
+        [HttpPost("form")]
+    
+}
